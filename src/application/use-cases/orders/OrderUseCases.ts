5144035a4d9ff5053986deceb2ce4520ab62da8d
Willian Santos <willian.s@develops.today>
2025-05-18 14:26:54 -0300

    docs: added comments to the code to provide a better understanding

--- src/application/use-cases/orders/OrderUseCases.ts
+++ src/application/use-cases/orders/OrderUseCases.ts
@@ -13,11 +13,14 @@ export class OrderUseCases {
     private readonly bookRepo: BookRepository,
   ) {}
 
+  // Cria um novo pedido com status pendente e calcula o total com base nos preços atuais dos livros
   async create({ itens, cliente }: CreateOrderInput): Promise<Order> {
+    // Busca todos os livros relacionados aos itens do pedido
     const books = await Promise.all(
       itens.map((item) => this.bookRepo.findOne(item.bookId)),
     );
 
+    // Calcula o total do pedido com base nos preços e quantidades
     let total = 0;
     books.forEach((book, index) => {
       const item = itens[index];
@@ -25,11 +28,13 @@ export class OrderUseCases {
       total += book.preco * item.quantidade;
     });
 
+    // Prepara os dados de associação dos itens do pedido
     const orderItemsData = itens.map((item) => ({
       book: { connect: { id: item.bookId } },
       quantidade: item.quantidade,
     }));
 
+    // Cria o pedido no repositório
     return await this.orderRepo.create({
       cliente,
       total,
@@ -42,6 +47,7 @@ export class OrderUseCases {
     return this.orderRepo.findAll();
   }
 
+  // Atualiza o status de um pedido, realizando validações e ajustes no estoque se necessário
   async updateStatus(
     orderId: number,
     status: "pago" | "cancelado",
@@ -49,20 +55,24 @@ export class OrderUseCases {
     const order = await this.orderRepo.findById(orderId);
     if (!order) throw new Error("Pedido não encontrado");
 
+    // Caso o status seja pago ou cancelado, lança uma exceção
     if (order.status === "pago" || order.status === "cancelado") {
       throw new Error("Pedido ja foi pago ou cancelado");
     }
 
+    // Caso o status seja cancelado, apenas atualiza o status da ordem
     if (status === "cancelado") {
       await this.orderRepo.update(orderId, { status });
       return;
     }
 
     if (status === "pago") {
+      // Busca os livros envolvidos no pedido
       const books = await Promise.all(
         order.itens.map((item) => this.bookRepo.findOne(item.bookId)),
       );
 
+      // Valida se há estoque suficiente para cada item
       books.forEach((book, index) => {
         const item = order.itens[index];
         if (!book || book.estoque < item.quantidade) {
@@ -70,6 +80,7 @@ export class OrderUseCases {
         }
       });
 
+      // Atualiza o estoque dos livros
       await Promise.all(
         books.map((book, index) => {
           const item = order.itens[index];
@@ -81,6 +92,7 @@ export class OrderUseCases {
         }),
       );
 
+      // Atualiza o status do pedido para pago
       await this.orderRepo.update(orderId, {
         status: "pago",
       });
