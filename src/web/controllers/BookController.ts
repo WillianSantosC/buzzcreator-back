c193ac046470e11e08dce48c72c7c297eb452264
Willian Santos <willian.s@develops.today>
2025-05-18 14:20:16 -0300

    refactor: changed error handling on book controller

--- src/web/controllers/BookController.ts
+++ src/web/controllers/BookController.ts
@@ -1,47 +1,47 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { BookUseCases } from "../../application/use-cases/books/BookUseCases";
 import { BookService } from "../../infrastructure/services/BookService";
 
 const bookUseCases = new BookUseCases(new BookService());
 
 export class BookController {
-  async create(req: Request, res: Response, next: NextFunction) {
+  async create(req: Request, res: Response) {
     try {
       const book = await bookUseCases.create(req.body);
       res.status(201).json(book);
-    } catch (err) {
-      next(err);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
   }
 
-  async list(_: Request, res: Response, next: NextFunction) {
+  async list(_: Request, res: Response) {
     try {
       const books = await bookUseCases.list();
       res.json(books);
-    } catch (err) {
-      next(err);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
   }
 
-  async update(req: Request, res: Response, next: NextFunction) {
+  async update(req: Request, res: Response) {
     try {
       const { id } = req.params;
 
       const book = await bookUseCases.update(Number(id), req.body);
       res.json(book);
-    } catch (err) {
-      next(err);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
   }
 
-  async delete(req: Request, res: Response, next: NextFunction) {
+  async delete(req: Request, res: Response) {
     try {
       const { id } = req.params;
 
       await bookUseCases.delete(Number(id));
       res.status(204).send();
-    } catch (err) {
-      next(err);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
     }
   }
 }
